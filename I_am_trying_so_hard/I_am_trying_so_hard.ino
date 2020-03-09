f6e2b068fb515c566f852eb4fd8e8cf3cfe73fd5
Caroline Smith <csmit757@students.bju.edu>
2020-03-08 21:05:55 -0400

    Functioning properly

--- I_am_trying_so_hard/I_am_trying_so_hard.ino
+++ I_am_trying_so_hard/I_am_trying_so_hard.ino
@@ -13,6 +13,7 @@ Algorithm for Bluetooth setup and transmission based off example at "https://git
 #include <ZSharpIR.h>
 #include <MLX90393.h>
 
+// These files will be included when Bluetooth is used
 //#include <ArduinoLowPower.h>
 //#include <ArduinoBLE.h>
 //#include <WiFi.h>
@@ -20,6 +21,13 @@ Algorithm for Bluetooth setup and transmission based off example at "https://git
 //#include <WiFiServer.h>
 //#include <WiFiUdp.h>
 
+// Define the IR and magnetic sensor
+ZSharpIR ir = ZSharpIR(A2, 1080);
+
+MLX90393 magnet;
+MLX90393::txyz data;
+
+//Global variables to be accessed
 int distance;
 
 //Normal magnetic conditions, found experimentally
@@ -27,15 +35,11 @@ float norm_x = 0;
 float norm_y = 0;
 float norm_z = 0;
 
-float allowed_diff_mag = 20;
+float allowed_diff_x = 0;
+float allowed_diff_y = 0;
+float allowed_diff_z = 0;
 int allowed_distance = 450;
 bool car_present = false;
-
-// Create a new instance of the SharpIR class:
-ZSharpIR ir = ZSharpIR(A2, 1080);
-
-MLX90393 magnet;
-MLX90393::txyz data;
 
 int t = 0;
 
@@ -48,34 +52,49 @@ void setup() {
   magnet.setOverSampling(0);
   magnet.setDigitalFiltering(0);
 
+ // WiFi.end();
+ 
   // Find the usual magnetic field for this area
   calibrate();
-
- // WiFi.end();
-
 }
 
 void loop() {
-
+ 
   distance = ir.distance();
-  Serial.println("IR: " + String(distance));
 
   // If there is any object within a certain distance of the sensor, check the magnetometer; if not, there is not a car present
   if (distance < allowed_distance) {
 
-        //Check if the magnetic field reading 10 times and see if it is within the allowed distance from normal
+        //Average the magnetic field for 20 cycles, ten times, and see if it is within the allowed distance from normal
         int loop_count = 0;
         int mag_check_count = 0;
-        
+
+        float sum_x = 0;
+        float sum_y = 0;
+        float sum_z = 0;
+
+        float avg_x;
+        float avg_y;
+        float avg_z;
+              
         while (loop_count < 10) {
-          
             //Read the magnetic field from the magnetometer and check if it is within allowed difference
-            magnet.readData(data);
+              
+            for (int i = 0; i < 20; i++){
+                magnet.readData(data);
+                sum_x += data.x;
+                sum_y += data.y;
+                sum_z += data.z;
+            }
+            
+            avg_x = sum_x / 20;
+            avg_y = sum_y / 20;
+            avg_z = sum_z / 20;
 
             // Check if the data is within the allowed distances from normal
-            if (abs(data.x - norm_x) > allowed_diff_mag || abs(data.y - norm_y) > allowed_diff_mag || abs(data.z - norm_z) > allowed_diff_mag) {
-              mag_check_count++;            
-              delay(500);
+            if (abs(avg_x - norm_x) > allowed_diff_x || abs(avg_y - norm_y) > allowed_diff_y || abs(avg_z - norm_z) > allowed_diff_z) {  
+              mag_check_count++;
+              delay(100);
             }
             else {
               mag_check_count = 0;
@@ -97,6 +116,7 @@ void loop() {
     car_present = false;
   }
 
+  // Print whether a car was found; this is for testing only
   if (car_present){
     Serial.println("taken");
   }
@@ -141,8 +161,13 @@ void calibrate() {
     sum_z += data.z;
   }
 
-  norm_x = sum_x / 10;
-  norm_y = sum_y / 10;
-  norm_z = sum_z / 10;
+  norm_x = sum_x / 20;
+  norm_y = sum_y / 20;
+  norm_z = sum_z / 20;
+
+  //Set the allowed difference to be 20% of each value
+  allowed_diff_x = norm_x/5;
+  allowed_diff_y = norm_y/5;
+  allowed_diff_z = norm_z/5;
  
 }
